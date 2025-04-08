270584604553aca26b831a01b3ea7cb1156ce098
FrederikTheDuck <pujolunai@proton.me>
2025-04-08 13:51:36 +0200

    CPPOOP

--- CPP/OOP/PROJ/Proj1.cpp
+++ CPP/OOP/PROJ/Proj1.cpp
@@ -8,25 +8,15 @@ class Item {
         string Author;
     //
     public:
-        void setTitle(string title) {
-            Title = title;
-        }
+        void setTitle(string title) { Title = title; }
         //
-        void setAuthor(string author) {
-            Author = author;
-        }
+        void setAuthor(string author) { Author = author; }
         //
-        string getTitle() {
-            return Title;
-        }
+        string getTitle() { return Title; }
         //
-        string getAuthor() {
-            return Author;
-        }
+        string getAuthor() { return Author; }
         //
-        void printDetails() {
-            cout << "Title: " << Title << "\nAuthor: " << Author << "\n";
-        }
+        void printDetails() { cout << "Title: " << Title << "\nAuthor: " << Author << "\n"; }
 };
 //
 class Book : public Item {
@@ -40,9 +30,7 @@ class Book : public Item {
             Pages = pages;
         }
         //
-        void printDetails() {
-            cout << "Title: " << Title << "\nAuthor: " << Author << "\nNumber of pages: " << Pages << "\n";
-        }
+        void printDetails() { cout << "Title: " << Title << "\nAuthor: " << Author << "\nNumber of pages: " << Pages << "\n"; }
 };
 //
 class Magazine : public Item {
@@ -50,18 +38,43 @@ class Magazine : public Item {
         int IssueNumber;
     //
     public:
-        Book(string title, string author, int issueNumber) {
+        Magazine(string title, string author, int issueNumber) {
             Title = title;
             Author = author;
             IssueNumber = issueNumber;
         }
         //
-        void printDetails() {
-            cout << "Title: " << Title << "\nAuthor: " << Author << "\nIssue Number: " << IssueNumber << "\n";
-        }
+        void printDetails() { cout << "Title: " << Title << "\nAuthor: " << Author << "\nIssue Number: " << IssueNumber << "\n"; }
 };
 //
-
+class Member {
+    private:
+        string Name;
+        int Id;
+    //
+    public:
+        void setName(string name) { Name = name; }
+        //
+        void setId(int id) { Id = id; }
+        //
+        string getName() { return Name; }
+        //
+        int getId() { return Id; }
+};
+//
+class Student : public Member {
+    private:
+        string GradeLevel;
+    //
+    public:
+        Student(string name, int id, string gradeLevel) {
+            Name = name;
+            Id = id;
+            GradeLevel = gradeLevel;
+        }
+        //
+        void printMemberDetails() { cout << "Name: " << Name << "\nId: " << Id << "\nGrade Level: " << GradeLevel << "\n"; }
+};
 //
 int main() {
     return 0;
